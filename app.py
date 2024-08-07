d54034f2200e895dfaadac35392726eaf5c155bd
shakedasido <shakedasido@gmail.com>
2024-08-07 15:09:27 +0300

    updated main

--- app.py
+++ app.py
@@ -89,7 +89,8 @@ def home():
 
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Default to port 5000 if PORT not set
+    app.run(host='0.0.0.0', port=port, debug=True)
+
 
